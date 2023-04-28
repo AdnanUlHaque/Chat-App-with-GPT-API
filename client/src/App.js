8ce43158472ff5e669c968ecef4dbfd2482c4936
Adnan Ul Haque <117963791+AdnanUlHaque@users.noreply.github.com>
2023-04-28 19:58:39 +0530

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,37 @@
+import { Routes, Route } from "react-router-dom";
+import { useMemo } from "react";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast";
+import { themeSettings } from "./theme";
+import Navbar from "./components/Navbar";
+import Homepage from "./pages/Homepage";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
+import ChatBot from "./pages/ChatBot";
+import ScifiImage from "./pages/ScifiImage";
+import Footer from "./components/Footer";
+
+function App() {
+  const theme = useMemo(() => createTheme(themeSettings()), []);
+
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Navbar />
+        <Toaster />
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/chatbot" element={<ChatBot />} />
+          <Route path="/scifi-image" element={<ScifiImage />} />
+        </Routes>
+        <Footer />
+      </ThemeProvider>
+    </>
+  );
+}
+
+export default App;
